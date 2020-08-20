c40b37e0ac1815d879646c5c67648d32945d1ab9
demon <mdosama0007@gmail.com>
2020-08-20 22:44:56 +0530

    new

---
+++ src/PracticeProblems/P1_TwoSum.java
@@ -0,0 +1,30 @@
+package PracticeProblems;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P1_TwoSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        sc.nextLine();
+        int[] n=new int[x];
+        for(int i=0;i<x;i++)
+            n[i]=sc.nextInt();
+        Arrays.sort(n);
+        for (int i=0;i<x-2;i++){
+            int j=i+1;
+            int k=x-1;
+            while (j < k) {
+                if ((n[j] + n[k]) +n[i]> 0)
+                    k--;
+                else if ((n[j] + n[k]) +n[i]< 0)
+                    j++;
+                else if ((n[j] + n[k]) +n[i]== 0){
+                    System.out.print("Triplet is " + n[i] + ", " + n[j] + ", " + n[k]);
+                    break;
+                }
+            }
+        }
+    }
+}
