8b53aa01043b5daa394bddd06c10b730a351cdc1
demon <mdosama0007@gmail.com>
2020-08-30 16:38:06 +0530

    new

--- src/lists/linkedLists/MainLinkedList.java
+++ src/lists/linkedLists/MainLinkedList.java
@@ -4,10 +4,20 @@ import java.util.*;
 
 public class MainLinkedList {
     public static void main(String[] args) {
-        List<Integer> ll=new LinkedList<>();
-        ll.add(12);
-        ll.add(2);
-        ll.add(32);
-        System.out.println(ll);
+        List<Integer> ll=new LinkedList<>(); //we can change it to ArrayList and there will be no error in operations
+        //after changing elements will be treated as the object of arraylist
+        List<Integer> al=new ArrayList<>();
+        getTimeDiff(ll);
+        getTimeDiff(al);
+
+    }
+    static void getTimeDiff(List<Integer> list){
+        long start=System.currentTimeMillis();
+        for (int i=0;i<100000;i++){
+            list.add(0,i);
+        }
+        long end=System.currentTimeMillis();
+        System.out.println(list.getClass().getName()+
+                "-->"+(end-start));
     }
 }
