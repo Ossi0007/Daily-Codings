b50bb870735b1dc3ebd1bc9da1bfff63b85153c9
demon <mdosama0007@gmail.com>
2020-08-30 17:05:11 +0530

    new

---
+++ src/lists/linkedLists/MainMyLinkedList.java
@@ -0,0 +1,14 @@
+package lists.linkedLists;
+
+public class MainMyLinkedList {
+    public static void main(String[] args) {
+        myLinkedList myLl=new myLinkedList();
+
+        for (int i=0;i<10;i++){
+            myLl.add(i*i);
+        }
+
+        myLl.print();
+
+    }
+}
